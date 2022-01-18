58e4d05c621e8d078e8803de79b2d12dfe9b7298
Dasha <2023stanovova.do@student.letovo.ru>
2022-01-18 14:38:05 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code int[] numbers1={936,489,845,959,550,687,776,604,244,694,546,322,753,1000,294,18,405,271,550,759};
+        //int[] numbers2={989,463,472,730,1,419,591,185,884,318,547,222,694,71,468,451,310,407,498,132};
+        //String[] words1={"wave", "room", "gentle", "search", "true", "board", "fowl", "upbeat", "name", "hug", "vengeful", "observe"};
+        //for (int i=0; i<numbers1[i];i++){
+        //    int a=numbers1[i];
+        //    numbers1[i]=numbers1[numbers1.length-i];
+        //    
+    }
+}
